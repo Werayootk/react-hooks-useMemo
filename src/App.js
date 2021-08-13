78f5233ece0588ac6425849c5c73e248e5e58dc8
Werayootk <werayoot5800@gmail.com>
2021-08-13 17:59:59 +0700

    Done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import MemoCase1 from './useMemoCase1/MemoCase1';
+
+function App() {
+  return (
+    <div className="App">
+      <MemoCase1 />
+    </div>
+  );
+}
+
+export default App;
